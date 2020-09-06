f1b614ae0934277fa630a2251f770477819b37b3
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-06 18:42:12 +0200

    printing out the list of job titles containing specific word

--- job_web_scraper.py
+++ job_web_scraper.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://justjoin.it/krakow/python/junior'
+URL = 'https://nofluffjobs.com/pl/jobs/krakow?criteria=city%3Dkrakow&page=1'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print(soup)
+#print(soup)
 
-results = soup.find('title')
-print(results)
+job_titles = soup.find_all('h4')
+
+for title in job_titles:
+    title = title.text
+    title = title.split()
+    if "internship" in title or "intern" in title:
+        title=' '.join(title)
+        print(title)
+       
