2db311cdef499da592ade773be95239aed04cbc2
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-06 18:55:52 +0200

    looping through my key words list

--- job_web_scraper.py
+++ job_web_scraper.py
@@ -4,16 +4,19 @@ from bs4 import BeautifulSoup
 URL = 'https://nofluffjobs.com/pl/jobs/krakow?criteria=city%3Dkrakow&page=1'
 page = requests.get(URL)
 
+my_key_words = ["intern", "internship", "staz", "staż", "praktyka"]
+
 soup = BeautifulSoup(page.content, 'html.parser')
 
 #print(soup)
 
 job_titles = soup.find_all('h4')
 
-for title in job_titles:
-    title = title.text
-    title = title.split()
-    if "internship" in title or "intern" in title:
-        title=' '.join(title)
-        print(title)
+for word in my_key_words:
+    for title in job_titles:
+        title = title.text
+        title = title.split()
+        if word in title:
+            title=' '.join(title)
+            print(title)
        
