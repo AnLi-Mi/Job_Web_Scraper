32c285259c606ae93d4041b1a3ff029ae8278285
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-07 21:28:41 +0200

    adding job details to the results

--- job_web_scraper.py
+++ job_web_scraper.py
@@ -16,16 +16,16 @@ while i<15:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     #filtering only job titles which are all h4
-    job_titles = soup.find_all('h4')
+    jobs = soup.find_all('a')
     # turning the strings of job titles to lists to search for my key words in them
-    for title in job_titles:
-        title = title.text
-        title = title.split()
+    for job in jobs:
+        job = job.text
+        job = job.split()
         #looping through all my key words
         for word in my_key_words:
-            if word in title:
-                title=' '.join(title)
-                print(title)
+            if word in job:
+                job=' '.join(job)
+                print(job)
     #moving to the next result page        
     i=int(i)
     i+=1
