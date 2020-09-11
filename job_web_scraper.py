2b5c140c00d194a59181aaada9495a2a478165d0
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-11 20:54:36 +0200

    duplicating code for a function for another website

--- job_web_scraper.py
+++ job_web_scraper.py
@@ -111,6 +111,101 @@ def scraping_nofluffjobs(number_of_pages):
 
 scraping_nofluffjobs(14)
 
+def scraping_JJIT():
+    
+
+    url = 'https://justjoin.it/all/python'
+    #scraping results for each page
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    #filtering only elementswhich are <a> - like clickable job offer 
+    jobs = soup.find_all('a')
+
+    #looping theough the results of <a> elements
+    for job in jobs:
+          
+            
+        # scraping ad's main information from first 'div' element
+        name=job.find_all('div', class_="css-rmb95w")
+        for n in name:
+            title = n.find('div', class_="css-1x9zltl")
+            company=n.find('div', class_="css-ajz12e")
+            salary = n.find('span', class_="css-112rr0w")
+            technology = n.find('div', class_="tag css-1g8us6r")
+            region = n.find('div', class_="css-1ihx907")
+            # spliting the scrped region into a list so I can loop through its individual words later
+            region = region.text
+            region= region.split()
+                
+        # spliting the scrped job ad into a list so I can loop through its individual words later
+        job = job.text  
+        job = job.split()
+
+
+        # filter no1 - Junior positions (from 'permanent_key_words' list) in specific locations (from 'locations' list)
+
+        #looping through the ad's list of words to find a my key words for Junior position  
+        for word in permanent_key_words:
+            if word in job:
+                for location in locations:
+                    # comparing location element with scraped city name of the ad
+                    if  location == region[0]:
+                    # joining the job and region into string again so it dispaly better in 'print'
+                        job=' '.join(job)
+                        region=' '.join(region)
+                        # printing the details of the filtered ads
+                        try:
+                            print (title.text, company.text)
+                        except AttributeError:
+                            print (" No info")
+                        try:
+                            print (f' Salary: {salary.text}')
+                        except AttributeError:
+                            print (" Salary: No info")
+                        try:
+                            print (f' Technologies: {technology.text}')
+                        except AttributeError:
+                            print (" Technologies: No info")
+                        try:
+                            print (f' Region: {region}', end='\n'*2)
+                        except AttributeError:
+                            print (" Region: No info", end='\n'*2)
+
+            
+        # filter no2 - Internship positions (from 'intern_key_words' list) in any location
+
+        #looping through the ad's list of words to find a my key words for Intern position
+        for word in intern_key_words:
+            if word in job:
+            # joining the job and region into string again so it dispaly better in 'print'
+                job=' '.join(job)
+            # printing the details of the filtered ads
+                try:
+                    print (title.text, company.text)
+                except AttributeError:
+                    print (" No info")
+                try:
+                    print (f' Salary: {salary.text}')
+                except AttributeError:
+                    print (" Salary: No info")
+                try:
+                    print (f' Technologies: {technology.text}')
+                except AttributeError:
+                    print (" Technologies: No info")
+                try:
+                    print (f' Region: {region.text}', end='\n'*2)
+                except AttributeError:
+                    print (" Region: No info", end='\n'*2)
+
+
+print ('....................')
+
+scraping_JJIT()
+
+
+                      
+                    
+        
 
 
 
