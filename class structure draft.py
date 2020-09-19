a929c6a0f8ce6bfabc85720404c3acfd8f8a5d6f
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-19 21:56:04 +0200

    managed to update objects attributes through called methods

--- class structure draft.py
+++ class structure draft.py
@@ -1,27 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-class Jobs:
 
-    def __init__ (self, url):
-        self.name = name
-        self.url = url
         
 
-    def jobs_fetch(self):
+    
+        
+class NoFluffJobs():
+
+    def __init__ (self, url, job_all_details, position, company, technology, salary, region):
+        self.url = url
+        self.job_all_details = job_all_details
+        self.position = position
+        self.company = company
+        self.salary = salary
+        self.technology = technology
+        self.region = region
+
+    def job_fetch(self):
     
         #scraping results for each page
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
         #filtering only elementswhich are <a> - like clickable job offer 
-        jobs = soup.find('a')
-        return jobs
-        
-class NoFluffJobs(Jobs):
-
-    def __init__ (self, job_all_details):
-        self.url = "https://nofluffjobs.com/pl/jobs/python?criteria=python&page="
-        self.job_all_details = jobs_fetch(self)
+        job = soup.find('a', class_="posting-list-item posting-list-item--businessIntelligence highlightId-ZGZ7TSHK")
+        self.job_all_details = job
+        return self.job_all_details
 
     def jobs_details_scraping(self):                
                 
@@ -31,36 +35,38 @@ class NoFluffJobs(Jobs):
             global title
             title = n.find('h4')
             title= title.text
+            self.position = title
             global company
             company=n.find('span')
             company = company.text
+            self.company = company
                     
                 #scraping ad's more specific details from second 'div' element
-        details =job.find_all('div', class_="posting-info position-relative d-none d-lg-flex flex-grow-1")
+        details =self.job_all_details.find_all('div', class_="posting-info position-relative d-none d-lg-flex flex-grow-1")
 
         for d in details:
             global salary
             salary = d.find('span', class_="text-truncate badgy salary btn btn-outline-secondary btn-sm")
             salary =salary.text
-            global technology
-            technology = d.find('object')
-            technology = technology.text
+            self.salary = salary
+            #global technology
+            #technology = d.find('object')
+            #technology = technology.text
+            #self.technology = technology
             global region
             region = d.find('span', class_="posting-info__location d-flex align-items-center ml-auto")
             # spliting the scrped region into a list so I can loop through its individual words later
             region = region.text
             region= region.split()
+            self.region = region
                          
-        return [title, company, salary, technology, region]
+        return self.position, self.company, self.salary, self.technology, self.region
 
-    def __init__ (self, position, company, technology, salary, region):
-        self.position = position
-        self.company = company
-        self.salary = salary
-        self.technology = technology
-        self.region = region
+    
+job1 = NoFluffJobs("https://nofluffjobs.com/pl/jobs/python?criteria=python&page","job_all_details3", "position1", "company1", "technology1", "salary1", "region1")
+job3 = NoFluffJobs("https://nofluffjobs.com/pl/jobs/python?criteria=python&page","job_all_details3","position3", "company3", "technology3", "salary3", "region3")
 
 
+NoFluffJobs.job_fetch(job3)
+NoFluffJobs.jobs_details_scraping(job3)
 
-job1 = NoFluffJobs("position1", "company1", "technology1", "salary1", "region1")
-job2 = NoFluffJobs(jobs_details_scraping(), "company1", "technology1", "salary1", "region1")
