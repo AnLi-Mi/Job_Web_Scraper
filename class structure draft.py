ceb61bd5c3c1201c3670112ca95ebe9024ea8bed
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-22 14:06:04 +0200

    assinging the job objects to variables, needs automatization

--- class structure draft.py
+++ class structure draft.py
@@ -4,8 +4,7 @@ import numpy as np
 
 
 class Jobs:
-    def __init__ (self, name, position, company, technology, salary, region):
-        self.name = name
+    def __init__ (self, position, company, technology, salary, region):
         self.position = position
         self.company = company
         self.salary = salary
@@ -14,7 +13,7 @@ class Jobs:
 
     def __str__(self):
      
-        return f"{name+i},\n Title: {title},\n Company: {company},\n Salary: {salary},\n Technology: {technology},\n Region: {region}\n\n\n"
+        return f" Title: {self.position},\n Company: {self.company},\n Salary: {self.salary},\n Technology: {self.technology},\n Region: {self.region}\n\n\n"
     
 
             
@@ -125,79 +124,97 @@ class NoFluffJobs():
 
     def job_objects_generator(self):
 
+        print ("JOB OFFERS FROM NOFLUFFJOBS SITE:")
+          
         job_objects_list = []
-        i = 1
-        name = "job"
 
+        i = 1
+        
         for job in self.my_jobs_list:
-            
+            global title
             title = job[0]
             try:
                 title = title.text.strip()
             except AttributeError:
                 title = "No Info"
-            
+                
+            global company
             company = job[1]
             try:
                 company= company.text.strip()
             except AttributeError:
                 company = "No Info"
-                
+
+            global salary    
             salary = job[2]
             try:
                 salary= salary.text.strip()
             except AttributeError:
                 salary = "No Info"
-                
+
+            global technology    
             technology = job[3]
             try:
                 technology = technology.text.strip()
             except AttributeError:
                 technology = "No Info"
-                
+
+            global region    
             region = job[4]
             try:
                 region = region.text.strip()
             except AttributeError:
                 region = "No Info"
 
-            i=str(i)
+            
                    
 
-            print (f"{name+i},\n Title: {title},\n Company: {company},\n Salary: {salary},\n Technology: {technology},\n Region: {region}\n\n\n")
+            print (f" JOB no {i}:\n Title: {title},\n Company: {company},\n Salary: {salary},\n Technology: {technology},\n Region: {region}\n\n\n")
 
-            x = Jobs(name+i, title, company, technology, salary, region)
+            x = Jobs(title, company, technology, salary, region)
             job_objects_list.append(x)
 
-            i=int(i)
             i=i+1
 
-           
-        return print (job_objects_list), print(type(job_objects_list[0]))
+        global job1
+        job1=job_objects_list[0]
+        global job2
+        job2=job_objects_list[1]
+        global job3
+        job3=job_objects_list[2]
+        global job4
+        job4=job_objects_list[3]
+        global job5
+        job5=job_objects_list[4]
+        global job6
+        job6=job_objects_list[5]
+        global job7
+        job7=job_objects_list[6]
+        global job8
+        job8=job_objects_list[7]
+        global job9
+        job9=job_objects_list[8]
+        global job10
+        job10=job_objects_list[9]
+        global job11
+        job11=job_objects_list[10]
+        global job12
+        job12=job_objects_list[11]
+        global job13
+        job13=job_objects_list[12]
 
-        
-
-            
                 
+        return job_objects_list, job1, job2, job3, job4, job5, job6, job7, job8, job9, job10, job11, job12, job13
 
-        
-            
-            
-            
-            
-            
-
-    
+           
 job_site1= NoFluffJobs()
-
-
 
 NoFluffJobs.a_element_fetch(job_site1,18)
 NoFluffJobs.jobs_details_scraping(job_site1)
 NoFluffJobs.junior_and_intern_jobs_filter(job_site1)
 NoFluffJobs.job_objects_generator(job_site1)
 
-print (type(job_site1))
+
 
 
 
