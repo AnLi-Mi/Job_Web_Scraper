573228b4f735b885dad99243ae01eaf89937cb50
AnLi-Mi <a.lipiarz@gmail.com>
2020-09-25 19:56:43 +0200

    added a name atribute to the job objects

--- job_web_scraping_object_orientated.py
+++ job_web_scraping_object_orientated.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 class Jobs:
-    def __init__ (self, position, company, technology, salary, region):
+    def __init__ (self, name, position, company, technology, salary, region):
+        self.name = name
         self.position = position
         self.company = company
         self.salary = salary
@@ -13,7 +14,7 @@ class Jobs:
 
     def __str__(self):
      
-        return f" Title: {self.position},\n Company: {self.company},\n Salary: {self.salary},\n Technology: {self.technology},\n Region: {self.region}\n\n\n"
+        return f"{self.name}\n Title: {self.position},\n Company: {self.company},\n Salary: {self.salary},\n Technology: {self.technology},\n Region: {self.region}\n\n\n"
     
 class JobSites():
 
@@ -41,8 +42,7 @@ class JobSites():
             
             i = int(i)
             i = i+1
-
-                
+ 
         return a_list
 
     
@@ -170,6 +170,9 @@ class NoFluffJobs(JobSites):
         for job in my_jobs_list:
             global title
             title = job[0]
+            i = str(i)
+            name = "Job "+i
+            i = int(i)
             try:
                 title = title.text.strip()
             except AttributeError:
@@ -207,40 +210,20 @@ class NoFluffJobs(JobSites):
 
             print (f" JOB no {i}:\n Title: {title},\n Company: {company},\n Salary: {salary},\n Technology: {technology},\n Region: {region}\n\n\n")
 
-            x = Jobs(title, company, technology, salary, region)
+            x = Jobs(name, title, company, technology, salary, region)
             job_objects_list.append(x)
 
             i=i+1
+            
+        #for job_object in job_objects_list:
+        #    global job1
+         #   job1=job_object
 
-        global job1
-        job1=job_objects_list[0]
-        global job2
-        job2=job_objects_list[1]
-        global job3
-        job3=job_objects_list[2]
-        global job4
-        job4=job_objects_list[3]
-        global job5
-        job5=job_objects_list[4]
-        global job6
-        job6=job_objects_list[5]
-        global job7
-        job7=job_objects_list[6]
-        global job8
-        job8=job_objects_list[7]
-        global job9
-        job9=job_objects_list[8]
-        global job10
-        job10=job_objects_list[9]
-        global job11
-        job11=job_objects_list[10]
-        global job12
-        job12=job_objects_list[11]
-        global job13
-        job13=job_objects_list[12]
+        for job_object in job_objects_list:
+            print (job_object)
 
                 
-        return job_objects_list, job1, job2, job3, job4, job5, job6, job7, job8, job9, job10, job11, job12, job13
+        #return
 
            
 job_site1= NoFluffJobs()
